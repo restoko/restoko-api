956a7c1261db10ec505bc0ed3607f215bcf21867
Alleo Indong <freedom_war550@live.com.ph>
2016-01-16 19:08:47 +0800

    updated create users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('user_groups_id')->unsigned();
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->rememberToken();
