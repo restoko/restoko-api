88a47114a745b94d3ba4557b8d7b1b0513070db8
Alleo Indong <freedom_war550@live.com.ph>
2016-01-26 01:20:08 +0800

    creating of a new cart

--- app/Http/Controllers/Pos/CartsController.php
+++ app/Http/Controllers/Pos/CartsController.php
@@ -7,22 +7,44 @@ use App\Models\Table;
 
 class CartsController extends ApiController
 {
-    public function createCart($tableId)
+    /**
+     * Create a new cart
+     *
+     * @param $tableId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createNewCart($tableId)
     {
         // Check if table is available
         if (! $this->tableAvailable($tableId)) {
             return $this->responseBadRequest(['Table is not available']);
         }
 
-        // Create New Cart
-        $data = [
-            'table_id'  => $tableId,
-            'status'    => Table::OCCUPIED
-        ];
+        // Update the table status
+        if ($this->makeTableOccupied($tableId)) {
+            return $this->responseBadRequest(['Table not found']);
+        }
 
-        $cart = Cart::create($data);
+        // Create New Cart
+        $cart = $this->createCart($tableId);
 
         return $this->responseOk($cart);
+    }
+
+    /**
+     * Create the cart
+     *
+     * @param $tableId
+     * @return static
+     */
+    private function createCart($tableId)
+    {
+        $data = [
+            'table_id'  => $tableId,
+            'status'    => Cart::ACTIVE
+        ];
+
+        return Cart::create($data);
     }
 
     /**
@@ -42,5 +64,19 @@ class CartsController extends ApiController
         }
 
         return true;
+    }
+
+    /**
+     * Make the table occupied
+     *
+     * @param $tableId
+     * @return mixed
+     */
+    private function makeTableOccupied($tableId)
+    {
+        $table = Table::where('id', $tableId)
+            ->update(['status' => Table::OCCUPIED]);
+
+        return $table;
     }
 }
