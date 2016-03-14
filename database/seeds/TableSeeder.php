45ab9c44d8d6185c28c9369f7894d06145c959cd
Alleo Indong <freedom_war550@live.com.ph>
2016-03-14 23:46:31 +0800

    updated tableseeder

--- database/seeds/TableSeeder.php
+++ database/seeds/TableSeeder.php
@@ -11,14 +11,126 @@ class TableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i < 10; $i++) {
-            \App\Models\Table::create(
-                [
-                    'name'  => 'table '.$i,
-                    'slug'  => 'table-'.$i,
-                    'status'    => 'available'
-                ]
-            );
-        }
+        \App\Models\Table::create(
+            [
+                'name'  => 'table 1',
+                'slug'  => 'table-1',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table 2',
+                'slug'  => 'table-2',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table 3',
+                'slug'  => 'table-3',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table 4',
+                'slug'  => 'table-4',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table 5',
+                'slug'  => 'table-5',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table vip-1',
+                'slug'  => 'table-vip-1',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table vip-2',
+                'slug'  => 'table-vip-2',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table o-1',
+                'slug'  => 'table-o-1',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table o-2',
+                'slug'  => 'table-o-2',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table o-3',
+                'slug'  => 'table-o-3',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table b-1',
+                'slug'  => 'table-b-1',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table b-2',
+                'slug'  => 'table-b-2',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table b-3',
+                'slug'  => 'table-b-3',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table b-4',
+                'slug'  => 'table-b-4',
+                'status'    => 'available'
+            ]
+        );
+
+        \App\Models\Table::create(
+            [
+                'name'  => 'table b-5',
+                'slug'  => 'table-b-5',
+                'status'    => 'available'
+            ]
+        );
+
+
     }
 }
